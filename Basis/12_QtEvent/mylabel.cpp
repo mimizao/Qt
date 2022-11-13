5ec02c281d98c117720b7074fc7a6ebc89bbe3ae
mimizao <88256191@qq.com>
2022-11-13 16:56:32 +0800

    QtEvent MouseEvent

---
+++ Basis/12_QtEvent/mylabel.cpp
@@ -0,0 +1,54 @@
+#include "mylabel.h"
+#include <QDebug>
+#include <QMouseEvent>
+MyLabel::MyLabel(QWidget *parent)
+    : QLabel{parent} {
+    // 设置鼠标追踪
+    setMouseTracking(true);
+}
+
+
+
+// 鼠标进入
+void MyLabel::enterEvent(QEvent *event) {
+    qDebug() << "enter";
+}
+
+// 鼠标离开
+void MyLabel::leaveEvent(QEvent *) {
+    qDebug() << "leave";
+}
+
+
+// 鼠标按下
+void MyLabel:: mousePressEvent(QMouseEvent *ev) {
+    if (ev->button() == Qt::LeftButton) {
+        QString str =  QString("press x = %1,y = %2").arg(ev->x()).arg(ev->y());
+        qDebug() << str;
+    }
+}
+// 鼠标释放
+void MyLabel:: mouseReleaseEvent(QMouseEvent *ev) {
+    if (ev->button() == Qt::LeftButton) {
+        QString str = QString("release global x = %1, global y = %2").arg(ev->globalX()).arg(ev->globalY());
+        qDebug() << str;
+    }
+}
+// 鼠标移动,注意这里需要鼠标按下了之后在移动才行
+void MyLabel:: mouseMoveEvent(QMouseEvent *ev) {
+    // 因为移动是一个持续的状态，所以再像上面那么写就不行了，有可能会存在偷换按键的情况
+//    if (ev->button() == Qt::LeftButton) {
+//        QString str = QString("move x = %1, y = %2").arg(ev->x()).arg(ev->y());
+//        qDebug() << str;
+//    }
+
+    // 如果是状态的操作的话就需要用这种按位与的判断才行
+//    if (ev->buttons() & Qt::LeftButton) {
+//        QString str = QString("move x = %1, y = %2").arg(ev->x()).arg(ev->y());
+//        qDebug() << str;
+//    }
+
+    // 如果设置了鼠标追踪就不需要按下才能生效
+    QString str = QString("move x = %1, y = %2").arg(ev->x()).arg(ev->y());
+    qDebug() << str;
+}
---
+++ Basis/12_QtEvent/mylabel.h
@@ -0,0 +1,29 @@
+#ifndef MYLABEL_H
+#define MYLABEL_H
+
+#include <QLabel>
+#include <QEvent>
+class MyLabel : public QLabel {
+    Q_OBJECT
+  public:
+    explicit MyLabel(QWidget *parent = nullptr);
+
+    // 鼠标进入
+    void enterEvent(QEvent *event);
+    // 鼠标离开
+    void leaveEvent(QEvent *);
+
+    // 鼠标按下
+    virtual void mousePressEvent(QMouseEvent *ev);
+    // 鼠标释放
+    virtual void mouseReleaseEvent(QMouseEvent *ev);
+    // 鼠标移动,注意这里需要鼠标按下了之后在移动才行
+    virtual void mouseMoveEvent(QMouseEvent *ev);
+
+
+
+  signals:
+
+};
+
+#endif // MYLABEL_H
