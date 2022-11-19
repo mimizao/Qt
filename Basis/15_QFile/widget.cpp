3c5fdd05ca00fd6179749b301fe6422ac355f8bc
mimizao <88256191@qq.com>
2022-11-19 20:05:13 +0800

    QFile

--- Basis/15_QFile/widget.cpp
+++ Basis/15_QFile/widget.cpp
@@ -1,15 +1,29 @@
 #include "widget.h"
 #include "ui_widget.h"
-
+#include <QFileDialog>
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
-    , ui(new Ui::Widget)
-{
+    , ui(new Ui::Widget) {
     ui->setupUi(this);
+
+    // 点击选取文件按钮，弹出文件对话框
+    connect(ui->pushButton, &QPushButton::clicked, this, [ = ]() {
+        QString path = QFileDialog::getOpenFileName(this, "open file", "‪C:\\Users\\Sven\\Desktop");
+        // 将路径放入到lineEdit中
+        ui->lineEdit->setText(path);
+
+        // 读取内容，放入到textEdit中
+        QFile file(path);
+        file.open(QIODevice::ReadOnly);
+        QByteArray arr = file.readAll();
+        ui->textEdit->setText(arr);
+        file.close();
+    });
+
+
 }
 
-Widget::~Widget()
-{
+Widget::~Widget() {
     delete ui;
 }
 
